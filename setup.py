d0d13be85bb0636ca7e28e264f4bb3e2f32828a7
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-12-14 01:54:19 +0000

    Update requirements for upcomping 2.4.0 release

--- setup.py
+++ setup.py
@@ -15,12 +15,12 @@ setup(
     author_email="tjelvar.olsson@jic.ac.uk",
     url=url,
     install_requires=[
-        "dtoolcore==2.9.0",
+        "dtoolcore==2.9.3",
         "dtool-cli==0.6.0",
-        "dtool-create==0.10.0",
+        "dtool-create==0.11.0",
         "dtool-info==0.7.0",
         "dtool-symlink==0.1.2",
-        "dtool-irods==0.4.0",
+        "dtool-irods==0.4.1",
     ],
     download_url="{}/tarball/{}".format(url, version),
     license="MIT"
