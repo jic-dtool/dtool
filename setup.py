7f65eb0362d252f85f3efca494328b8fd2f1ae26
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-02-09 12:44:13 +0000

    Update requirements for 3.2.0 release

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ setup(
     install_requires=[
         "dtoolcore==3.1.0",
         "dtool-cli==0.6.0",
-        "dtool-create==0.13.0",
+        "dtool-create==0.14.0",
         "dtool-info==0.9.0",
-        "dtool-symlink==0.1.2",
+        "dtool-symlink==0.2.0",
         "dtool-irods==0.5.0",
         "dtool-s3==0.1.1",
     ],
