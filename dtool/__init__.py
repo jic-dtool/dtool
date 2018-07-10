a3e691919638a991de24e42fce1fc941bba0410b
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-07-10 16:38:07 +0100

    Update version number to 3.6.2

--- dtool/__init__.py
+++ dtool/__init__.py
@@ -1,3 +1,3 @@
 """dtool package."""
 
-__version__ = "3.6.1"
+__version__ = "3.6.2"
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 url = "https://github.com/jic-dtool/dtool"
-version = "3.6.1"
+version = "3.6.2"
 readme = open('README.rst').read()
 
 setup(
