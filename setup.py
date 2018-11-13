6398b191cc0be67e797c2c33cab4c7483a6dd355
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-11-13 14:21:12 +0000

    Update requirements for upcoming 3.13.0 release

--- setup.py
+++ setup.py
@@ -15,12 +15,13 @@ setup(
     author_email="tjelvar.olsson@jic.ac.uk",
     url=url,
     install_requires=[
-        "dtoolcore==3.7.0",
+        "dtoolcore==3.8.0",
         "dtool-cli==0.7.0",
         "dtool-create==0.21.0",
         "dtool-info==0.12.0",
         "dtool-symlink==0.3.0",
         "dtool-http==0.2.0",
+        "dtool-config==0.1.0",
     ],
     download_url="{}/tarball/{}".format(url, version),
     license="MIT"
