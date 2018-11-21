9de8f098a1509de1117eaa0d04aeeb4831e7936a
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-11-21 21:28:34 +0000

    Update requirements

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ setup(
     install_requires=[
         "dtoolcore==3.8.0",
         "dtool-cli==0.7.0",
-        "dtool-create==0.21.0",
-        "dtool-info==0.12.0",
+        "dtool-create==0.22.0",
+        "dtool-info==0.13.0",
         "dtool-symlink==0.3.0",
         "dtool-http==0.2.0",
         "dtool-config==0.1.0",
