6ce94ea291a9cce596488c16296f78fac5584451
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2019-10-31 11:09:34 +0000

    Update version number to 3.20.0

--- dtool/__init__.py
+++ dtool/__init__.py
@@ -1,3 +1,3 @@
 """dtool package."""
 
-__version__ = "3.19.0"
+__version__ = "3.20.0"
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 url = "https://github.com/jic-dtool/dtool"
-version = "3.19.0"
+version = "3.20.0"
 readme = open('README.rst').read()
 
 setup(
