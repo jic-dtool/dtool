a3dc61c9687abd8d68d95ae11ce83add57891d55
Tjelvar Olsson <olssont@nbi.ac.uk>
2020-02-28 15:20:19 +0000

    Update requirements

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ setup(
     author_email="tjelvar.olsson@jic.ac.uk",
     url=url,
     install_requires=[
-        "dtoolcore==3.16.2",
+        "dtoolcore==3.16.3",
         "dtool-cli==0.7.0",
-        "dtool-create==0.22.0",
+        "dtool-create==0.23.0",
         "dtool-info==0.16.1",
         "dtool-symlink==0.3.0",
         "dtool-http==0.4.0",
