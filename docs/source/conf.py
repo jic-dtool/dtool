5f48f9e3dcda20f73349b6c295962f30ea37f96c
Tjelvar Olsson <olssont@nbi.ac.uk>
2020-03-24 17:22:04 +0000

    Update version number to 3.25.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ repo_name = u"dtool"
 # built documents.
 #
 # The short X.Y version.
-version = u"3.24.0"
+version = u"3.25.0"
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- dtool/__init__.py
+++ dtool/__init__.py
@@ -1,3 +1,3 @@
 """dtool package."""
 
-__version__ = "3.24.0"
+__version__ = "3.25.0"
